89fdbd47f60cddd2b911d9dc53f2a25ca22edb6e
DiegoAlirioGonzalezM <da.gonzalezm1@uniandes.edu.co>
2026-04-16 13:42:59 -0500

    HU-001-02: Definir modelo de datos album

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.vinilosapp"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
