8d340cc4235f1c2f5404e90c6d2d7dd413abef33
DiegoAlirioGonzalezM <da.gonzalezm1@uniandes.edu.co>
2026-04-16 16:05:40 -0500

    HU-001-02: Definir modelo de datos album (#76)

---
+++ app/src/main/java/com/example/vinilosapp/data/model/Album.kt
@@ -0,0 +1,16 @@
+package com.example.vinilosapp.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Album(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String,
+    @SerializedName("cover") val cover: String,
+    @SerializedName("performers") val performers: List<Performer>?,
+    @SerializedName("tracks") val tracks: List<Track>?,
+    @SerializedName("comments") val comments: List<AlbumComment>?,
+    @SerializedName("releaseDate") val releaseDate: String?,
+    @SerializedName("description") val description: String?,
+    @SerializedName("genre") val genre: String?,
+    @SerializedName("recordLabel") val recordLabel: String?
+)
